a8b0dfb0f181d4ee9a578682d29cb8b01ce2e616
Ani <animebtm@gmail.com>
2026-05-11 00:42:41 +0530

    back to api keys

--- app/utils/supabase/client.js
+++ app/utils/supabase/client.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.BASE_URL,
-  process.env.NON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
